a53cf1e6cd06017a6e058296f3150a904c62e861
Emilio Barrera Gonzalez <emilio.barrera1997@gmail.com>
2020-12-02 16:45:14 -0600

    finalizado main

--- index.php
+++ index.php
@@ -17,8 +17,34 @@
 
                 <div class = "col-md-9 main-content">
                     <div class = "row">
-                        
+                        <div class = "col-md-3 p-principal text-center">
+                            <img src="Assets/img/Página Principal.png" alt="">
+                        </div>
                     </div>
+                    <div class = "row text-center">
+                        <div class = "col-md-12 flash-rep">
+                            <img src="Assets/img/representacion_flash.png" alt="">
+                        </div>
+                    </div>
+
+                    <div class = "row text-left">
+                        <div class = "col-md-12 bloque-content">
+                            <img src="Assets/img/bloque 1 content.png" alt="">
+                        </div>
+                    </div>
+
+                    <div class = "row text-left">
+                        <div class = "col-md-12 bloque-content">
+                            <img src="Assets/img/bloque 2 content.png" alt="">
+                        </div>
+                    </div>
+
+                    <div class = "row text-left">
+                        <div class = "col-md-12 bloque-content">
+                            <img src="Assets/img/bloque 3 content.png" alt="">
+                        </div>
+                    </div>
+
                 </div>
 
             </div>
