380afeea0c07e165658dc04499f852317810cf4f
Fernando Garcia <fgarcia@scriptech.com>
2020-12-01 21:16:29 -0600

    header, archivo  de scripts y estilos

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require "view/linkscss.php";?>
+</head>
+<body>
+    <div class="container">
+        <!-- HEADER -->
+        <?php require "view/header.php"?>
+    </div>
+<?php require "view/linksjs.php";?>
+</body>
+</html>
