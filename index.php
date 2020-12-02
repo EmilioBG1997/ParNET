498f926da425c6d80a92adfb919c3bb1f9b53b0f
Fernando Garcia <fgarcia@scriptech.com>
2020-12-01 22:15:13 -0600

    Integracion de plantilla

--- index.php
+++ index.php
@@ -9,6 +9,12 @@
     <div class="container">
         <!-- HEADER -->
         <?php require "view/header.php"?>
+
+        <!-- ASIDE -->
+        <?php require "view/aside.html"?>
+
+        <!-- FOOTER -->
+        <?php require "view/footer.php"?>
     </div>
 <?php require "view/linksjs.php";?>
 </body>
