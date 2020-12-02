14120d8febca09b3e231a97a77d6fa56ca1c4ac6
Emilio Barrera Gonzalez <emilio.barrera1997@gmail.com>
2020-12-02 16:48:57 -0600

    arreglados bugs de responsividad en main

--- index.php
+++ index.php
@@ -18,30 +18,30 @@
                 <div class = "col-md-9 main-content">
                     <div class = "row">
                         <div class = "col-md-3 p-principal text-center">
-                            <img src="Assets/img/Página Principal.png" alt="">
+                            <img src="Assets/img/Página Principal.png" class="img-fluid" alt="">
                         </div>
                     </div>
                     <div class = "row text-center">
                         <div class = "col-md-12 flash-rep">
-                            <img src="Assets/img/representacion_flash.png" alt="">
+                            <img src="Assets/img/representacion_flash.png" class="img-fluid" alt="">
                         </div>
                     </div>
 
                     <div class = "row text-left">
                         <div class = "col-md-12 bloque-content">
-                            <img src="Assets/img/bloque 1 content.png" alt="">
+                            <img src="Assets/img/bloque 1 content.png" class="img-fluid" alt="">
                         </div>
                     </div>
 
                     <div class = "row text-left">
                         <div class = "col-md-12 bloque-content">
-                            <img src="Assets/img/bloque 2 content.png" alt="">
+                            <img src="Assets/img/bloque 2 content.png" class="img-fluid" alt="">
                         </div>
                     </div>
 
                     <div class = "row text-left">
                         <div class = "col-md-12 bloque-content">
-                            <img src="Assets/img/bloque 3 content.png" alt="">
+                            <img src="Assets/img/bloque 3 content.png" class="img-fluid" alt="">
                         </div>
                     </div>
 
