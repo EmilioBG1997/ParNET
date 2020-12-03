5feb789ff73af365a789007f49eb403af48aa731
Emilio Barrera Gonzalez <emilio.barrera1997@gmail.com>
2020-12-03 01:03:44 -0600

    agregado modelo contacto

---
+++ model/contacto.php
@@ -0,0 +1,58 @@
+<?php
+require "connection.php";
+
+class Contacto extends DbConnection{
+
+    public function __construct(){
+        parent::__construct();
+        $this->contacto = Array();
+    }
+
+    public function createContacto($nombre, $email, $empresa, $telefono){
+        $nombre = $this->conn->real_escape_string($nombre);
+        $email = $this->conn->real_escape_string($email);
+        $empresa = $this->conn->real_escape_string($empresa);
+        $telefono = $this->conn->real_escape_string($telefono);
+        $sql = "INSERT INTO CONTACTO (NOMBRE, CORREO, EMPRESA, TELEFONO) VALUES ('$nombre', '$email', '$empresa', $telefono)";
+        $ejecucion = mysqli_query($this->conn,$sql) or die (mysqli_error($this->conn));
+        if(mysqli_affected_rows($this->conn)){
+            return "Exito al insertar";
+        }
+    }
+
+    public function readContactos(){
+        $sql = "SELECT * FROM CONTACTO;";
+        $ejecucion = mysqli_query($this->conn,$sql) or die (mysqli_error($this->conn));
+        if(mysqli_num_rows($ejecucion) > 0){
+            while($row = $ejecucion->fetch_assoc()){
+                $this->contacto["data"][] = $row;
+            }
+            return json_encode($this->contacto);
+        }
+        return json_encode($this->contacto);
+    }
+
+    public function deleteContacto($id){
+        $id = $this->conn->real_escape_string($id);
+        $sql = "DELETE FROM CONTACTO WHERE IDCONTACTO = $id";
+        $resultado = mysqli_query($this->conn,$sql) or die (mysqli_error($this->conn));
+        if($resultado){
+            return "Exito al borrar el registro $id";
+        }
+    }  
+
+    public function updateContacto($id, $nombre, $correo, $empresa, $telefono)
+    {
+        $id = $this->conn->real_escape_string($id);
+        $nombre = $this->conn->real_escape_string($nombre);
+        $empresa = $this->conn->real_escape_string($empresa);
+        $telefono = $this->conn->real_escape_string($telefono);
+        $correo = $this->conn->real_escape_string($correo);
+        $sql = "UPDATE CONTACTO SET NOMBRE = '$nombre' ,EMPRESA = '$empresa' ,TELEFONO = $telefono, CORREO = '$correo' WHERE IDCONTACTO = '$id'";
+        $resultado = mysqli_query($this->conn,$sql) or die (mysqli_error($this->conn));
+        if($resultado){
+            return "Exito al actualizar el registro $id";
+        }
+    }
+}
+?>
