ad2f1ddf6913e5b8e853e79881c939628b670e60
Emilio Barrera Gonzalez <emilio.barrera1997@gmail.com>
2020-12-03 13:41:18 -0600

    agregado ajax a contacto

--- view/contactos.php
+++ view/contactos.php
@@ -3,28 +3,28 @@
     <p>Por favor rellena el siguiente formulario, nos contactaremos contigo tan pronto como sea posible.</p>
     <div class = "container container-form">
         <div class = "row">
-            <form action="#" class = "form-contacto">
+            <form class = "form-contacto" id = "form-contacto">
                 <div class = "col-md-12 form-group">
                     <label for="nombre">Nombre</label>
-                    <input type="text" class = "form-control" aria-describedby="nombre" maxlength="45">
+                    <input type="text" class = "form-control" aria-describedby="nombre" maxlength="45" id ="nombre">
                     <small id= "nombre">Ingresa aqui tu nombre</small>
                 </div>
                 <div class = "col-md-12 form-group">
                     <label for="email">Email</label>
-                    <input type="email" class = "form-control" aria-describedby="email" maxlength="50">
-                    <small id= "nombre">Ingresa aqui tu email</small>
+                    <input type="email" class = "form-control" aria-describedby="email" maxlength="50" id ="email">
+                    <small id= "email">Ingresa aqui tu email</small>
                 </div>
 
                 <div class = "col-md-12 form-group">
                     <label for="empresa">Empresa</label>
-                    <input type="text" class = "form-control" maxlength="45">
-                    <small id= "nombre">Ingresa aqui tu empresa</small>
+                    <input type="text" class = "form-control" maxlength="45" id ="empresa">
+                    <small id= "empresa">Ingresa aqui tu empresa</small>
                 </div>
 
                 <div class = "col-md-12 form-group">
                     <label for="empresa">Telefono</label>
-                    <input type="tel" class = "form-control" maxlength="10">
-                    <small id= "nombre">Ingresa tu telefono a diez digitos</small>
+                    <input type="tel" class = "form-control" maxlength="10" id ="telefono">
+                    <small id= "telefono">Ingresa tu telefono a diez digitos</small>
                 </div>
                 <div class="col-md-12">
                     <p>
@@ -37,5 +37,7 @@
             </form>
         </div>
     </div>
+    <script src = "Assets/js/contacto.js">
 
+    </script>
 </div>
