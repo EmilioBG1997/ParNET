893b0e4669002711bd1c98c2ab3f14540304b198
Emilio Barrera Gonzalez <emilio.barrera1997@gmail.com>
2020-12-02 18:02:31 -0600

    preparacion para cargado dinamico de paginas

--- index.php
+++ index.php
@@ -15,37 +15,11 @@
             <div class  ="row row-cols-12">
                 <?php require "view/aside.php"?>
 
-                <div class = "col-md-9 main-content">
-                    <div class = "row">
-                        <div class = "col-md-3 p-principal text-center">
-                            <img src="Assets/img/Página Principal.png" class="img-fluid" alt="">
-                        </div>
-                    </div>
-                    <div class = "row text-center">
-                        <div class = "col-md-12 flash-rep">
-                            <img src="Assets/img/representacion_flash.png" class="img-fluid" alt="">
-                        </div>
-                    </div>
+                <div class = "col-md-9 main-content" id="main"> 
+                    <?php require "view/main.php";?>        
+                </div> <!-- main -->
 
-                    <div class = "row text-left">
-                        <div class = "col-md-12 bloque-content">
-                            <img src="Assets/img/bloque 1 content.png" class="img-fluid" alt="">
-                        </div>
-                    </div>
 
-                    <div class = "row text-left">
-                        <div class = "col-md-12 bloque-content">
-                            <img src="Assets/img/bloque 2 content.png" class="img-fluid" alt="">
-                        </div>
-                    </div>
-
-                    <div class = "row text-left">
-                        <div class = "col-md-12 bloque-content">
-                            <img src="Assets/img/bloque 3 content.png" class="img-fluid" alt="">
-                        </div>
-                    </div>
-
-                </div>
 
             </div>
         </div>
---
+++ view/main.php
@@ -0,0 +1,28 @@
+                <div class = "row">
+                        <div class = "col-md-3 p-principal text-center">
+                            <img src="Assets/img/Página Principal.png" class="img-fluid" alt="">
+                        </div>
+                    </div>
+                    <div class = "row text-center">
+                        <div class = "col-md-12 flash-rep">
+                            <img src="Assets/img/representacion_flash.png" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class = "row text-left">
+                        <div class = "col-md-12 bloque-content">
+                            <img src="Assets/img/bloque 1 content.png" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class = "row text-left">
+                        <div class = "col-md-12 bloque-content">
+                            <img src="Assets/img/bloque 2 content.png" class="img-fluid" alt="">
+                        </div>
+                    </div>
+
+                    <div class = "row text-left">
+                        <div class = "col-md-12 bloque-content">
+                            <img src="Assets/img/bloque 3 content.png" class="img-fluid" alt="">
+                        </div>
+                    </div>
