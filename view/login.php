fce6b72fb2cac515db95d0ebcdfdcae5a3d3847a
Emilio Barrera Gonzalez <emilio.barrera1997@gmail.com>
2020-12-03 12:30:10 -0600

    agregado login

---
+++ view/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Assets/css/login.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class = "container">
+        <div class = "login-box">
+            <div class = "login-img">
+                <img src="../Assets/img/logotipo.png" alt="">
+                <h1>INICIO DE SESION</h1>
+            </div>
+            <form action="">
+                <label for="email">Email</label>
+                <input type="email" name = "email" maxlength="45">
+
+                <label for="password">Contraseña</label>
+                <input type="password" name = "password" maxlenght="30">
+
+                <input type="submit" value ="ENVIAR">
+
+            </form>
+        </div>
+    </div>
+</body>
+</html>
